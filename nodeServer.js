8e02e18c2f1781626389fd452cd8ec0ef8e3d4cf
Gustavo M. R. Santos <gustavo.rosendo@b-plat.net>
2022-02-09 15:07:43 -0300

    - add fs + new routes for img and css

--- nodeServer.js
+++ nodeServer.js
@@ -1,4 +1,8 @@
+// http is native to NodeJS, we just need to ask for it
 const http = require('http');
+// fs = file system module which is built into NodeJS
+// fs gives Node access to THIS computer's file system
+const fs = require('fs');
 
 const SERVER_PORT = 3000;
 
@@ -6,21 +10,43 @@ const SERVER_PORT = 3000;
 // takes 1 arg:
 // 1. callback, which has 2 args: req, res
 const server = http.createServer((req, res) => {
-  console.log(req);
-  
-  // res = object who sends the response to the resquester
-  // http message:
-  // 1. start line - CHECK
-  // 2. header
-  // 3. body
+  console.log(req.url);
 
-  // writeHead takes 2 args:
-  // 1. status code
-  // 2. object for the mime-type
-  res.writeHead(200, {'content-type':'text/html'});
-  
-  // write the body of our response
-  res.write('<h1>Hello, world!</h1>');
+  if (req.url === '/') {
+    // res = object who sends the response to the resquester
+    // http message:
+    // 1. start line - CHECK
+    // 2. header
+    // 3. body
+
+    // writeHead takes 2 args:
+    // 1. status code
+    // 2. object for the mime-type
+    res.writeHead(200, {'content-type':'text/html'});
+    
+    // using sync method here just to avoid handling async issues for the sake of this sample
+    const homePageHTML = fs.readFileSync('node.html');
+
+    // write the body of our response
+    //res.write('<h1>This is the home page!</h1>');
+    res.write(homePageHTML);
+  }
+  else if (req.url === '/css/styles.css') {
+    res.writeHead(200, {'content-type':'text/css'});
+    const styles = fs.readFileSync('css/styles.css');
+    res.write(styles);
+  }
+  else if (req.url === '/assets/Node-logo.png') {
+    res.writeHead(200, {'content-type':'image/png'});
+    const image = fs.readFileSync('assets/Node-logo.png');
+    res.write(image);
+  }
+  else {
+    res.writeHead(404, {'content-type':'text/html'});
+    res.write('<h4 style=\'color: red\'>Sorry, page not found!</h4>');
+  }
+
+  // close the http connection
   res.end();
 });
 
