3a7468a6a4418ffea2473ff2bdaef57f1bed445e
Gustavo M. R. Santos <gustavo.rosendo@b-plat.net>
2022-02-09 14:12:33 -0300

    - hello world example

---
+++ nodeServer.js
@@ -0,0 +1,28 @@
+const http = require('http');
+
+const SERVER_PORT = 3000;
+
+// the http module has a createServer method
+// takes 1 arg:
+// 1. callback, which has 2 args: req, res
+const server = http.createServer((req, res) => {
+  console.log(req);
+  
+  // res = object who sends the response to the resquester
+  // http message:
+  // 1. start line - CHECK
+  // 2. header
+  // 3. body
+
+  // writeHead() takes 2 args:
+  // 1. status code
+  // 2. object for the mime-type
+  res.writeHead(200, {'content-type':'text/html'});
+  
+  // write the body of our response
+  res.write('<h1>Hello, world!</h1>');
+  res.end();
+});
+
+// createServer 
+server.listen(SERVER_PORT);
